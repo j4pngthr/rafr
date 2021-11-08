5ce15e1929e0ed1cfe8fc3c9bca3db4b3f767ed2
j4pngthr <weibeicun@gmail.com>
2021-11-08 17:00:15 +0900

    getFileHeadのデバッグ

--- crawdad_rafr_source/init.cpp
+++ crawdad_rafr_source/init.cpp
@@ -6,7 +6,7 @@ const int num_cache = 3, num_methods = 3, num_off = 5, num_simu = 1000, debg_flg
 static string file_head = "data/";
 const vector<int> vi({ DEGREE, BETWEENNESS, RAFR });
 vector<int> xs({ 1000, 5000, 10000, 50000, 100000 });
-// 出力の横軸 x plot 
+// 出力の横軸 x plot
 vector<double> x_p;
 // simulation pattern id
 int sim_pat_id;
@@ -18,7 +18,7 @@ enum sim_pat {
 };
 
 string getFileHead() {
-  return getFileHead();
+  return file_head;
 }
 
 string getSimPatStr() {
